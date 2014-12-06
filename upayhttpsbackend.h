9cfe3f77dac95b6c663ffd0ca8be046a0d5e6203
Simon Goller <neosam@posteo.de>
2014-12-06 14:13:01 +0100

    Comments for controller methods.

--- upayhttpsbackend.h
+++ upayhttpsbackend.h
@@ -22,8 +22,26 @@ private:
 public:
     explicit UpayHttpsBackend(QString server, quint16 port, QObject *parent = 0);
 
+    /**
+     * @brief Check if the tokens are valid.  Return valid tokens.
+     * @param tokens List of tokens which should be checked.
+     * @return Valid tokens.
+     */
     QList<Token> validate(const QList<Token> &tokens);
+
+    /**
+     * @brief Create new tokens which have the values specified in the list.
+     * @param valueList List of values.
+     * @return
+     */
     QList<Token> create(const QList<int> &valueList);
+
+    /**
+     * @brief Invalidate oldTokens and activate the newTokens.
+     * @param oldTokens Tokens to invalidate.
+     * @param newTokens Tokens to activate.
+     * @return
+     */
     QList<Token> transform(const QList<Token> &oldTokens, const QList<Token> &newTokens);
 
 signals:
--- walletmanager.h
+++ walletmanager.h
@@ -37,12 +37,52 @@ private:
 public:
     explicit WalletManager(Wallet *wallet, QObject *parent = 0);
 
+    /**
+     * @brief Create new transaction by removing tokens from the wallet
+     *
+     * The value must be smaller or equal to the balance of the wallet.
+     * If it is higher, this method will return a nullTransaction.
+     * It will communicate with the upay server if it cannot find the
+     * matching tokens for this value since it has break one token into two
+     * pieces.
+     *
+     * @param value The value of the transaction.
+     * @param comment An optional transaction comment.
+     * @return The transaction (can be nullTransaction).
+     */
     Transaction createTransaction(quint32 value, const QString &comment = "");
+
+    /**
+     * @brief Add value of the transaction to the wallet.
+     *
+     * This method will check if the token are valid.  If not it will return
+     * false.  Then it will transform all the tokens in one new token which
+     * is only known by the server and the app and will add the token to the
+     * wallet.
+     *
+     * @param transaction
+     * @return
+     */
     bool cashTransaction(const Transaction &transaction);
+
+    /**
+     * @brief Transforms a list of tokens into one new token
+     * @param tokenList
+     * @return
+     */
     Token transformTokens(const QList<Token> &tokenList);
 
 signals:
+    /**
+     * @brief Emitted when a transaction was successfully created.
+     * @param transaction
+     */
     void exportedTransaction(const Transaction &transaction);
+
+    /**
+     * @brief Emitted when a transaction was successfully imported.
+     * @param transaction
+     */
     void importedTransaction(const Transaction &transaction);
 
 public slots:
